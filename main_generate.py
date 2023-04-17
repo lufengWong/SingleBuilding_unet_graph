30394e4eca45bbaefeac8a41bff80b33d07b9387
lufeng Wong <18328395095@163.com>
2023-04-17 17:38:12 +0800

    done

--- main_generate.py
+++ main_generate.py
@@ -10,6 +10,8 @@ import os
 import sys
 import shutil
 
+import matplotlib.pyplot as plt
+
 sys.path.append(os.getcwd())
 sys.path.append(os.getcwd())
 sys.path.append(os.path.join(os.getcwd(), 'Part_1_Singlebuilding_point_unet_align'))
@@ -45,22 +47,68 @@ if __name__ == '__main__':
     # 输入参数
     # points_ploygon_input_1 = [[0, 5000], [10000, 5000], [10000, 0], [30000, 0], [30000, 20000], [0, 30000]]
 
-
     # name_project = 'typical-1'
     # points_ploygon_input_1 = [[0, 0], [22000, 0], [22000, 14000], [0, 14000],]
     # apartments_need_1 = {110: 2}
     # num_elevators_1 = 2
 
     #
-    name_project = 'typical-2'
-    points_ploygon_input_1 = [[0, 0], [34000, 0], [34000, 16000], [0, 16000], ]
-    apartments_need_1 = {90: 2, 80:2}
-    num_elevators_1 = 2
+    # name_project = 'typical-2'
+    # points_ploygon_input_1 = [[0, 0], [34000, 0], [34000, 16000], [0, 16000], ]
+    # apartments_need_1 = {90: 2, 80:2}
+    # num_elevators_1 = 2
     #
     #
     # name_project = 'typical-3'
     # points_ploygon_input_1 = [[0, 0], [38000, 0], [38000, 20000], [0, 20000], ]
     # apartments_need_1 = {100: 2, 60: 4}
+    # num_elevators_1 = 2
+
+    # name_project = 'untypical-1'
+    # points_ploygon_input_1 = [[0, 0], [28000, 0], [28000, 8000], [18000, 8000],[18000, 14000],[10000, 14000],[10000, 8000], [0,8000] ]
+    # apartments_need_1 = {120: 2, }
+    # num_elevators_1 = 2
+
+    name_project = 'untypical-2'
+    width=10000
+    length=20000
+    points_ploygon_input_1 = [[length/2,0],[length/2, width],[-length/2, width],[-length/2,0],
+                              [0,0],
+                              [0,-width],[length,-width],[length,0],[length/2,0]]
+
+    points_ploygon_input_1 = np.array(points_ploygon_input_1).reshape(-1, 2) \
+                             - np.array([[length/4, 0]])\
+                             + np.array([[256 / 2 * utils.size_grid, 256 / 2 * utils.size_grid]])
+
+    plt.plot(np.array(points_ploygon_input_1).reshape(-1, 2)[:, 0],
+             np.array(points_ploygon_input_1).reshape(-1, 2)[:, 1])
+    plt.show()
+    apartments_need_1 = {100: 2}
+    num_elevators_1 = 2
+
+    # name_project = 'untypical-3'
+    # height = 16000
+    # width = 10000
+    # points_ploygon_input_1 = [[width / 2 + height, 0], [width / 2 + height, width / 2], [width / 2, width / 2],
+    #                           [width / 2, width / 2 + height],
+    #
+    #                           [-width / 2, width / 2 + height], [-width / 2, width / 2],
+    #                           [-width / 2 - height, width / 2], [-width / 2 - height, 0],
+    #
+    #                           [-width / 2 - height, -width / 2], [-width / 2, -width / 2],
+    #                           [-width / 2, -width / 2 - height], [0, -width / 2 - height],
+    #
+    #                           [width / 2, -width / 2 - height], [width / 2, -width / 2, ],
+    #                           [width / 2 + height, -width / 2], [width / 2 + height, 0]
+    #                           ]
+    # # plt.Polygon(points_ploygon_input_1)
+    # points_ploygon_input_1 = np.array(points_ploygon_input_1).reshape(-1, 2) + np.array(
+    #     [[256 / 2 * utils.size_grid, 256 / 2 * utils.size_grid]])
+    # plt.plot(np.array(points_ploygon_input_1).reshape(-1, 2)[:, 0],
+    #          np.array(points_ploygon_input_1).reshape(-1, 2)[:, 1])
+    # plt.show()
+    #
+    # apartments_need_1 = {120: 4, }
     # num_elevators_1 = 2
 
     # # 项目名称
@@ -70,7 +118,7 @@ if __name__ == '__main__':
     path_data_save = r'C:\Users\Administrator\Desktop\singleBuilding_unet_graph\Data_temp'
 
     path_project = os.path.join(path_data_save, name_project)
-    name_files =['Boundary', 'Marks_txt', 'Marks_pkl', 'Segment', 'Lines']
+    name_files = ['Boundary', 'Marks_txt', 'Marks_pkl', 'Segment', 'Lines']
     for file in name_files:
         path_txt_save = os.path.join(path_project, file)
         if os.path.exists(path_txt_save):
@@ -92,11 +140,11 @@ if __name__ == '__main__':
         f.write('\n')
 
     # 1.2 保存 marker txt
-    boundary_input, list_markers =  stage_1_step_4_generate_input.get_mark_nodes(points_ploygon_input_1,
-                                                      apartments_need_1,
-                                                      num_elevators_1,
-                                                      paths_png_test_1,
-                                                      paths_graph_test_1)
+    boundary_input, list_markers = stage_1_step_4_generate_input.get_mark_nodes(points_ploygon_input_1,
+                                                                                apartments_need_1,
+                                                                                num_elevators_1,
+                                                                                paths_png_test_1,
+                                                                                paths_graph_test_1)
 
     for one, dict_loc in enumerate(list_markers):
         name_mark_ = 'mark_' + str(one) + '.txt'
@@ -153,9 +201,7 @@ if __name__ == '__main__':
         path_img = os.path.join(path_image_output, png)
         list_largest_boundary, list_rect, list_lines = find_region.from_png_2_polygon(path_img)
         txt_name = png.split('.')[0]
-        find_region.write_gemo_txt(list_largest_boundary, list_rect, list_lines, os.path.join(path_txt_line, txt_name + '.txt'))
+        find_region.write_gemo_txt(list_largest_boundary, list_rect, list_lines,
+                                   os.path.join(path_txt_line, txt_name + '.txt'))
 
     # rhino
-
-
-
